95df821d2f7e80f590dd8af285e671204016e388
Charles Heller <charles.heller@hotmail.de>
2026-04-06 13:04:36 +0200

    feat: navgation mobile

--- app-elikuren-web/app/auth/sign-in/[[...rest]]/page.tsx
+++ app-elikuren-web/app/auth/sign-in/[[...rest]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="relative top-0 flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="relative flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <SignIn
         appearance={{
           variables: {
